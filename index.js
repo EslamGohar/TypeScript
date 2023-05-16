bb7ae9c1675ea4b4196b32ed25c6d5973a4ec386
Eslam Gohar <islamgoher1@gmail.com>
2023-05-17 02:09:21 +0300

    some concepts of typescript

--- index.js
+++ index.js
@@ -1,104 +1,301 @@
-"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 // To compile down into JavaScript: `tsc fileName.ts`.
 // To compile the typescript code automatically, whenever you make a change: `tsc fileName.ts -w`.
 // To specify the name of the output file: `tsc index.ts --outfile fileName.js`.
 // Not explicitly state the type as TypeScript automatically infers the type of a variable (type inference).
-let sport = 'football';
-let id = 5;
-let personName = 'Danny';
+var sport = 'football';
+var id = 5;
+var personName = 'Danny';
 personName.toLowerCase();
 console.log(personName);
-let arr = [1, 3, 5, 7];
+var arr = [1, 3, 5, 7];
 arr.pop();
 console.log(arr);
 personName = 'Anna';
 // A union type is a variable that can be assigned more than one type.
-let age;
+var age;
 age = 26;
 age = '26';
-let options = [true, false, false];
-let books = [
+var options = [true, false, false];
+var books = [
     { name: 'Fooled by randomness', author: 'Nassim Taleb' },
     { name: 'Sapiens', author: 'Yuval Noah Harari' },
 ];
-let arrAny = ['hello', 1, true];
+var arrAny = ['hello', 1, true];
 // A tuple: is an array with fixed size and known datatypes.
-let person = ['Danny', 1, true];
+var person = ['Danny', 1, true];
 // person[0] = 100; // Error - Value at index 0 can only be a string.
 // Object in TypeScript
-let vehicle;
+var vehicle;
 vehicle = {
     type: 'Car',
     model: 'BMW',
     wheels: 4,
     flying: false,
 };
-let car = {
+var car = {
     type: 'BMW',
     model: 'x6',
     wheels: 4,
     flying: false
 };
-let bike = {
+var bike = {
     type: 'BMX',
     model: 'g9',
     wheels: 2,
     flying: false
 };
-let sayStuff = {
+var sayStuff = {
     sayHi: function (name) {
-        return `Hi ${name}!`;
+        return "Hi ".concat(name, "!");
     },
-    sayBye: (name) => `Bye ${name}!`
+    sayBye: function (name) { return "Bye ".concat(name, "!"); }
 };
 console.log(sayStuff.sayHi('Heisenberg'));
 console.log(sayStuff.sayBye('Heisenberg'));
 // Regular function
 function circle(diam) {
-    return `The Circumference is ${Math.PI * diam}`;
+    return "The Circumference is ".concat(Math.PI * diam);
 }
 console.log(circle(10));
 // Arrow function
-const circleDiameter = (diam) => `The Circumference is ${Math.PI * diam}`;
+var circleDiameter = function (diam) { return "The Circumference is ".concat(Math.PI * diam); };
 console.log(circleDiameter(10));
 // "?:" optional operator with union type |. 
-const add = (a, b, c) => {
+var add = function (a, b, c) {
     console.log(c);
     return a + b;
 };
 console.log(add(5, 4, 'I could pass a number, string, or nothing here!'));
 // A function that returns nothing is said to return `void` – a complete lack of any value.
-const logMessage = (message) => console.log(`This is the message: ${message}`);
+var logMessage = function (message) { return console.log("This is the message: ".concat(message)); };
 logMessage('TypeScript is superb');
-let sayHello;
-sayHello = (name) => {
+var sayHello;
+sayHello = function (name) {
     console.log('Hello ' + name);
 };
 sayHello('Eslam');
-const person1 = {
+var person1 = {
     name: 'John',
     id: 45
 };
-const sayHi = (person) => {
-    return `Hi ${person.name}`;
+var sayHi = function (person) {
+    return "Hi ".concat(person.name);
 };
 // DOM and Type Casting.
 /** TypeScript doesn't have access to the DOM like JavaScript.
 ** ERROR >> Object is possibly 'null'.
   TypeScript can't be sure the anchor tag exists, as it can't access the DOM
 **/
-const anchorLink = document.querySelector('a');
+var anchorLink = document.querySelector('a');
 console.log(anchorLink.href);
 /** TO SOLVE THIS for Accessing the DOM using TypeScript:
 * Using `Non-null assertion operator (!)`:
   can tell the compiler explicitly that an expression has value other than `null` or `undefined`.
   when the compiler cannot infer the type with certainty.
 **/
-const link = document.querySelector('a');
+var link = document.querySelector('a');
 console.log(link.href); // www.example.com
 /** Need to select a DOM element by its `class` or `id`?
 * TypeScript can't infer the type, as it could be anything.
 * TO SOLVE THIS, Use ``Type Casting``.
 **/
-const form = document.getElementById('signup-form');
+var form = document.getElementById('signup-form');
 console.log(form.method);
+// Classes in TypeScript
+var Person = /** @class */ (function () {
+    function Person(n, c, e, p) {
+        this.name = n;
+        this.isCool = c;
+        this.email = e;
+        this.pets = p;
+    }
+    // Can destructure the constructor to make the code more concise.
+    // constructor(
+    // 	readonly name: string;
+    // 	private isCool: boolean;
+    // 	protected email: string;
+    // 	public pets: number;
+    // ) {}
+    Person.prototype.sayMyName = function (name) {
+        console.log("Your not Heisenberg, you're ".concat(this.name));
+    };
+    return Person;
+}());
+var person2 = new Person('John', false, 'john@example.com', 2);
+console.log(person2.name); // John
+person2.name = 'James'; // Error - readonly property
+console.log(person2.email); // Error - protected property - only accessible within Person class and its subclasses
+console.log(person2.pets); // 2
+console.log(person2.isCool); // Error - private property - only accessible within Person class
+// Class can be extended
+var Programmer = /** @class */ (function (_super) {
+    __extends(Programmer, _super);
+    function Programmer(name, isCool, email, pets, pL) {
+        var _this = 
+        //The super call must supply all parameters for base (Person) class, as the constructor is not inherited.
+        _super.call(this, name, isCool, email, pets) || this;
+        _this.programmingLanguages = pL;
+        return _this;
+    }
+    return Programmer;
+}(Person));
+var dog = {
+    name: "Max",
+    tail: true
+};
+var bear = {
+    name: "Winnie",
+    honey: true,
+};
+var bear = {
+    name: "Winnie",
+    honey: true,
+};
+var human = {
+    name: "Henrik",
+    age: 40,
+    speak: function (sentence) { return console.log(sentence); }
+};
+var Preson = /** @class */ (function () {
+    function Preson(username, password) {
+        this.username = username;
+        this.password = password;
+    }
+    Preson.prototype.format = function () {
+        return this.username.toLocaleLowerCase();
+    };
+    return Preson;
+}());
+var preson1;
+var preson2;
+var people = [];
+preson1 = new Preson("GINA", "password123");
+preson2 = new Preson("Jane", "typescript");
+people.push(preson1);
+people.push(preson2);
+console.log(preson1.format()); // gina
+/** Generics: allow to create a component can work over a variety of types rather than single one.(more reusable)
+ * We can pass in any object to a Function, with properties and values the object has..
+ * by using a generic <T> - where `T` is the 'Type parameter'.
+**/
+var addID = function (obj) {
+    var id = Math.floor(Math.random() * 1000);
+    return __assign(__assign({}, obj), { id: id });
+};
+var p1 = addID({ name: 'John', age: 40 });
+var p2 = addID('Sally');
+console.log(p1.id); // 271
+console.log(p1.name); // John
+console.log(p2.id);
+console.log(p2.name); // ERROR: Property 'name' does not exist on type '"Sally" & { id: number; }'.
+// To solve this above problem, tell TypeScript only objects should be accepted, by making our generic type, T, an extension of object.
+var addIDs = function (object) {
+    var id = Math.floor(Math.random() * 1000);
+    return __assign(__assign({}, object), { id: id });
+};
+var pe1 = addID({ name: 'John', age: 40 });
+console.log(pe1.name); // John
+console.log(pe1.age); // 40
+function logLength(a) {
+    console.log(a.length);
+    return a;
+}
+var hello = "Hello World";
+logLength(hello); // 11
+var howMany = 8;
+logLength(howMany); // ERROR: numbers don't have length properties.
+/*---------------*/
+function logLengths(a) {
+    a.forEach(function (element) {
+        console.log(element.length);
+    });
+}
+var arrLengths = [
+    'This string has a length prop',
+    ['This', 'arr', 'has', 'length'],
+    { material: 'plastic', length: 30 },
+];
+logLengths(arrLengths); // 29 - 4 - 30
+var reg = {
+    name: 'John',
+    age: 48,
+    documents: ['passport', 'bank statement', 'visa'], // or can pass in any another type for documents 
+};
+/* Enums: allow us to define or declare a collection of related values,
+*  that can be numbers or strings, as a set of related named constants.
+** By default, enums are number based – they store string values as numbers. But they can also be strings.
+*/
+var ResourceType;
+(function (ResourceType) {
+    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
+    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
+    ResourceType[ResourceType["FILM"] = 2] = "FILM";
+    ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
+    ResourceType[ResourceType["PERSON"] = 4] = "PERSON";
+})(ResourceType || (ResourceType = {}));
+console.log(ResourceType.BOOK); // 0
+console.log(ResourceType.AUTHOR); // 1
+var Direction;
+(function (Direction) {
+    Direction["Up"] = "Up";
+    Direction["Right"] = "Right";
+    Direction["Down"] = "Down";
+    Direction["Left"] = "Left";
+})(Direction || (Direction = {}));
+console.log(Direction.Right); // Right
+console.log(Direction.Down); // Down
+/*** TypeScript Strict Mode:
+ * a couple of the things that strict mode does:
+    - no implicit any (noImplicitAny),
+    - strict null checks (strictNullCheck),
+  
+ ** With the noImplicitAny option = true,
+    TypeScript will instantly flag an error if we don't explicitly state the type of variable.
+ 
+ * When the strictNullChecks option = false,
+    TypeScript ignores null and undefined. This can lead to unexpected errors at runtime.
+
+ * With strictNullChecks set to true,
+    null and undefined have their own types, you'll get a type error if you assign them to a variable.
+***/
+// ERROR: Parameter 'a' implicitly has an 'any' type.
+function logName(a) {
+    console.log(a.name);
+}
+// ERROR: Object is possibly 'undefined'.
+var getSong = function () {
+    return 'song';
+};
+var whoSangThis = getSong();
+var singles = [
+    { song: 'touch of grey', artist: 'grateful dead' },
+    { song: 'paint it black', artist: 'rolling stones' },
+];
+var single = singles.find(function (s) { return s.song === whoSangThis; });
+console.log(single.artist); // if (single) {..} to check if it isn't null or undefined.
--- index.ts
+++ index.ts
@@ -56,7 +56,7 @@ vehicle = {
 // ERROR - missing the {type, flying} properties.
 
 
-// When defining the signature of an object, you will usually use an interface.
+// When defining the signature of an object, you will usually use an Interface.
 // If need to check multiple objects have the same specific properties and value types.
 interface Vehicle { 
 	type: string;
@@ -141,7 +141,6 @@ const sayHi = (person: PersonObject) => {
 	return `Hi ${person.name}`
 }
  
-
 // DOM and Type Casting.
 /** TypeScript doesn't have access to the DOM like JavaScript.
 ** ERROR >> Object is possibly 'null'. 
@@ -164,3 +163,296 @@ console.log(link.href); // www.example.com
 **/
 const form = document.getElementById('signup-form') as HTMLFormElement;
 console.log(form.method);
+
+// Classes in TypeScript
+class Person { 
+	readonly name: string; // This property is immutable - it can only be read
+	private isCool: boolean; // Can only access or modify from methods within this class
+	protected email: string; // Can access or modify from this class and subclasses
+	public pets: number; // Can access or modify from anywhere - including outside the class
+	
+	constructor(n: string, c: boolean, e: string, p: number) { 
+		this.name = n;
+		this.isCool = c;
+		this.email = e;
+		this.pets = p;
+	}
+	
+	// Can destructure the constructor to make the code more concise.
+	// constructor(
+	// 	readonly name: string;
+	// 	private isCool: boolean;
+	// 	protected email: string;
+	// 	public pets: number;
+	// ) {}
+	
+	sayMyName(name: string) { 
+		console.log(`Your not Heisenberg, you're ${this.name}`);
+	}
+}
+
+const person2 = new Person('John', false, 'john@example.com', 2);
+console.log(person2.name); // John
+person2.name = 'James'; // Error - readonly property
+console.log(person2.email); // Error - protected property - only accessible within Person class and its subclasses
+console.log(person2.pets); // 2
+console.log(person2.isCool); // Error - private property - only accessible within Person class
+
+// Class can be extended
+class Programmer extends Person { 
+	programmingLanguages: string[];
+	
+	constructor(
+		name: string,
+		isCool: boolean,
+		email: string,
+		pets: number,
+		pL: string[]
+	) {
+		//The super call must supply all parameters for base (Person) class, as the constructor is not inherited.
+		super(name, isCool, email, pets);
+		this.programmingLanguages = pL;
+	}
+}
+
+
+/** Interfaces vs. Type Aliases
+ * Interface can be extendable, can reopen to add new properties.
+ * Type cannot be reopened, and cannot be changed after being created. 
+**/
+
+interface Animal { 
+	name: string;
+}
+
+interface Animal { 
+	tail: boolean;
+}
+
+const dog: Animal = {
+	name: "Max",
+	tail: true
+}
+
+// Extending an interface
+interface Animal {
+  name: string
+}
+
+interface Bear extends Animal {
+  honey: boolean
+}
+
+const bear: Bear = {
+  name: "Winnie",
+  honey: true,
+}
+
+// Type aliases
+type Animal = {
+	name: string
+}
+
+type Animal = {
+	tail: boolean
+}
+// ERROR: Duplicate identifier 'Animal'.
+
+//Extending a type via intersections '&'
+type Animal = {
+  name: string
+}
+
+type Bear = Animal & {
+  honey: boolean
+}
+
+const bear: Bear = {
+  name: "Winnie",
+  honey: true,
+}
+
+// interface can define function signatures
+interface Human {
+	name: string,
+	age: number,
+	speak(sentence: string): void
+}
+
+const human: Human = {
+	name: "Henrik",
+	age: 40,
+	speak: sentence => console.log(sentence) 
+}
+
+/** NOTE:
+ * Why we would use an interface over a class in the above example?
+	Using an interface is that it is only used by TypeScript, not JavaScript. This means that it won't get compiled and add bloat to your JavaScript. Classes are features of JavaScript, so it would get compiled.
+	Interface is a structure used solely for type-checking.
+*/
+
+interface HasFormatter {
+	format(): string;
+}
+
+class Preson implements HasFormatter { 
+	constructor(public username: string, protected password: string) { }
+	
+	format() {
+		return this.username.toLocaleLowerCase();
+	}
+}
+
+let preson1: HasFormatter;
+let preson2: HasFormatter;
+let people: HasFormatter[] = [];
+
+preson1 = new Preson("GINA", "password123");
+preson2 = new Preson("Jane", "typescript");
+
+people.push(preson1);
+people.push(preson2);
+console.log(preson1.format()); // gina
+
+
+/** Generics: allow to create a component can work over a variety of types rather than single one.(more reusable)
+ * We can pass in any object to a Function, with properties and values the object has.. 
+ * by using a generic <T> - where `T` is the 'Type parameter'.
+**/
+const addID = <T>(obj: T) => {
+	let id = Math.floor(Math.random() * 1000);
+	
+	return {...obj, id};
+}
+let p1 = addID({ name: 'John', age: 40 });
+let p2 = addID('Sally');
+
+console.log(p1.id); // 271
+console.log(p1.name); // John
+
+console.log(p2.id);
+console.log(p2.name); // ERROR: Property 'name' does not exist on type '"Sally" & { id: number; }'.
+
+// To solve this above problem, tell TypeScript only objects should be accepted, by making our generic type, T, an extension of object.
+
+const addIDs = <T extends object>(object: T) => { 
+	let id = Math.floor(Math.random() * 1000);
+	return {...object, id};
+}
+
+let pe1 = addID<{ name: string; age: number }>({ name: 'John', age: 40 });
+console.log(pe1.name) // John
+console.log(pe1.age)	 // 40
+
+
+// a generic that extends an interface
+
+interface hasLength { 
+	length: number;
+}
+
+function logLength<T extends hasLength>(a: T) {
+	console.log(a.length)
+	return a;
+}
+
+let hello = "Hello World";
+logLength(hello) // 11
+
+let howMany = 8;
+logLength(howMany) // ERROR: numbers don't have length properties.
+/*---------------*/
+
+function logLengths<T extends hasLength>(a: T[]) { 
+	a.forEach((element) => { 
+		console.log(element.length);
+	})
+}
+
+let arrLengths = [
+	'This string has a length prop',
+  	['This', 'arr', 'has', 'length'],
+  	{ material: 'plastic', length: 30 },
+]
+
+logLengths(arrLengths);	// 29 - 4 - 30
+
+
+/* NOTE:
+* When we don't know what type a certain value in an object, we can use a generic <T> to pass in the type.
+*/
+// The type, T, will be passed in
+interface Register<T> {
+  name: string;
+  age: number;
+  documents: T;
+}
+
+const reg: Register<string[]> = {
+  name: 'John',
+  age: 48,
+  documents: ['passport', 'bank statement', 'visa'], // or can pass in any another type for documents 
+};
+
+
+/* Enums: allow us to define or declare a collection of related values,
+*  that can be numbers or strings, as a set of related named constants.
+** By default, enums are number based – they store string values as numbers. But they can also be strings.
+*/
+enum ResourceType {
+  BOOK,
+  AUTHOR,
+  FILM,
+  DIRECTOR,
+  PERSON,
+}
+
+console.log(ResourceType.BOOK); // 0
+console.log(ResourceType.AUTHOR); // 1
+
+enum Direction {
+  Up = 'Up',
+  Right = 'Right',
+  Down = 'Down',
+  Left = 'Left',
+}
+
+console.log(Direction.Right); // Right
+console.log(Direction.Down); // Down
+
+
+/*** TypeScript Strict Mode:
+ * a couple of the things that strict mode does: 
+ 	- no implicit any (noImplicitAny), 
+ 	- strict null checks (strictNullCheck),
+  
+ ** With the noImplicitAny option = true, 
+ 	TypeScript will instantly flag an error if we don't explicitly state the type of variable.
+ 
+ * When the strictNullChecks option = false,
+	TypeScript ignores null and undefined. This can lead to unexpected errors at runtime.
+
+ * With strictNullChecks set to true, 
+	null and undefined have their own types, you'll get a type error if you assign them to a variable.
+***/
+
+// ERROR: Parameter 'a' implicitly has an 'any' type.
+function logName(a) {
+  console.log(a.name);
+}
+
+// ERROR: Object is possibly 'undefined'.
+const getSong = () => {
+  return 'song';
+};
+
+let whoSangThis: string = getSong();
+
+const singles = [
+  { song: 'touch of grey', artist: 'grateful dead' },
+  { song: 'paint it black', artist: 'rolling stones' },
+];
+
+const single = singles.find((s) => s.song === whoSangThis);
+
+console.log(single.artist); // if (single) {..} to check if it isn't null or undefined.
